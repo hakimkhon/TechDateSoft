828a6498056d35f76e06f866947155654bfc2f74
hakimkhon <hakimkhon94@gmail.com>
2022-02-27 14:51:08 +0500

    new

--- markaz/settings.py
+++ markaz/settings.py
@@ -6,12 +6,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-rim_l+_5ao!u2kp1q22*57^(ftg2&qh+o+ug%4vde6k!0djyj%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
 ALLOWED_HOSTS = ['tech-date-soft.herokuapp.com','127.0.0.1']
 
